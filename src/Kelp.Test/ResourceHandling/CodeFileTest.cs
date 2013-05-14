965951187cb193deef51086ece271ea767a5fda7
Igor France <igor.france@gmail.com>
2013-05-14 14:57:32 +0200

    Initial commit

---
+++ src/Kelp.Test/ResourceHandling/CodeFileTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Kelp.Test.ResourceHandling
+{
+	using System;
+	using Kelp.ResourceHandling;
+
+	public abstract class CodeFileTest
+	{
+		static CodeFileTest()
+		{
+			Configuration.Current.TemporaryDirectory =
+				Utilities.MapPath(Configuration.Current.TemporaryDirectory);
+		}
+	}
+}
