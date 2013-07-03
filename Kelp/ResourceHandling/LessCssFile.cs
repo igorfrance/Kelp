74f7b798a6c898f3d572c25ab07011f939265bef
Igor France <igor.france@gmail.com>
2013-07-04 01:33:51 +0200

    Added support for LESS.

---
+++ Kelp/ResourceHandling/LessCssFile.cs
@@ -0,0 +1,28 @@
+﻿namespace Kelp.ResourceHandling
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	using dotless.Core.Parser;
+	using dotless.Core.Parser.Infrastructure;
+	using dotless.Core.Parser.Tree;
+
+	/// <summary>
+	/// Class CssLessFile
+	/// </summary>
+	[ResourceFile(ResourceType.LessCss, "text/css", "less")]
+	public class LessCssFile : CssFile
+	{
+		/// <inheritdoc/>
+		protected override string PostProcess(string sourceCode)
+		{
+			Parser parser = new Parser();
+			Ruleset result = parser.Parse(sourceCode, this.AbsolutePath);
+
+			sourceCode = result.ToCSS(new Env());
+			return base.PostProcess(sourceCode);
+		}
+	}
+}
---
+++ Kelp/ResourceHandling/ResourceFileAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace Kelp.ResourceHandling
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	/// <summary>
+	/// Class CodeFileAttribute
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class ResourceFileAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets the <see cref="ResourceType"/> associated with this <see cref="CodeFile"/>.
+		/// </summary>
+		public ResourceType ResourceType { get; private set; }
+		/// <summary>
+		/// Gets the content-type associated with this <see cref="CodeFile"/>.
+		/// </summary>
+		public string ContentType { get; private set; }
+		/// <summary>
+		/// Gets the file extensions associated with this <see cref="CodeFile"/>.
+		/// </summary>
+		public IEnumerable<string> Extensions { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ResourceFileAttribute"/> class.
+		/// </summary>
+		/// <param name="resourceType">The <see cref="ResourceType"/> of the code file.</param>
+		/// <param name="contentType">The content type of this code file.</param>
+		/// <param name="extensions">The extensions of this code file.</param>
+		public ResourceFileAttribute(ResourceType resourceType, string contentType, params string[] extensions)
+		{
+			this.ResourceType = resourceType;
+			this.ContentType = contentType;
+			this.Extensions = extensions;
+		}
+	}
+}
